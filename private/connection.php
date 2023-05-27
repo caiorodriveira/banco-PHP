ec5ad08046113c665b2b4cde966f895a1310d7b7
Caio Rodrigo <rodrigocaio2775@gmail.com>
2023-05-27 15:12:12 -0300

    REFACTOR - cria as tabelas no connection.php

--- private/connection.php
+++ private/connection.php
@@ -4,4 +4,19 @@
     define('USERNAME', 'root');
     define('PASSWORD', '');
     $conn = new PDO("mysql:host=".HOST.";dbname=".DBNAME.";charset=utf8", USERNAME, PASSWORD);
+
+    $conn->query("CREATE TABLE IF NOT EXISTS pessoa(
+        id_pessoa INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+        nome varchar(100) NOT NULL,
+        cpf varchar(11) NOT NULL unique,
+        email varchar(50) NOT NULL);
+        
+        create TABLE IF NOT EXISTS conta(
+        id_conta INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+        numero INT NOT NULL,
+        saldo INT,
+        id_pessoa INT NOT NULL,
+        CONSTRAINT fk_pessoa FOREIGN KEY(id_pessoa) REFERENCES pessoa(id_pessoa));
+        
+        INSERT INTO pessoa VALUES(DEFAULT, 'Caio Rodrigo' , 52377178880, 'caio@gmail.com');")
 ?>
